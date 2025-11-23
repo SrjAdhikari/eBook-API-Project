6c85a3177eee1d9a25ff4696bb50fe3880b19cfc
Suraj-Everlink <s.adhikari@everlink-corp.com>
2025-11-23 19:32:33 +0900

    add book model

---
+++ src/book/book.model.ts
@@ -0,0 +1,36 @@
+//* src/book/book.model.ts
+
+import mongoose from "mongoose";
+import { Book } from "./book.types";
+
+const { Schema } = mongoose;
+const bookSchema = new Schema<Book>(
+	{
+		title: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		author: {
+			type: Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+		},
+		genre: {
+			type: String,
+			required: true,
+		},
+		coverImage: {
+			type: String,
+			required: true,
+		},
+		file: {
+			type: String,
+			required: true,
+		},
+	},
+	{ timestamps: true }
+);
+
+const Book = mongoose.model("Book", bookSchema);
+export default Book;
