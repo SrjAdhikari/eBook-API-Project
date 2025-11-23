a4e65c613ad49753f5378c5f2b2305ee6fcc4e8d
Suraj-Everlink <s.adhikari@everlink-corp.com>
2025-11-23 21:06:07 +0900

    change file size to 10mb

--- src/middlewares/upload.middleware.ts
+++ src/middlewares/upload.middleware.ts
@@ -6,14 +6,14 @@ import multer from "multer";
  *
  * - `dest`: Absolute path where uploaded files will be stored
  *    (../../public/data/uploads relative to current file)
- * - `limits`: Maximum allowed file size (30MB)
+ * - `limits`: Maximum allowed file size (10MB)
  *
  * - Multer will automatically save incoming files to the specified directory
  *   and include metadata in `req.file` or `req.files`.
  */
 const upload = multer({
 	dest: path.resolve(__dirname, "../../public/data/uploads"),
-	limits: { fileSize: 3e7 },
+	limits: { fileSize: 1e7 },
 });
 
 export default upload;
