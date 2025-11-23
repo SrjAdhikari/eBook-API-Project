4bda7126a95f03c9559f4a88453137701902f61c
Suraj-Everlink <s.adhikari@everlink-corp.com>
2025-11-23 12:52:59 +0900

    add global error handler

---
+++ src/middlewares/error.middleware.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from "express";
+import { HttpError } from "http-errors";
+import config from "../config/config";
+
+// Global error handler middleware
+const globalErrorHandler = (
+	error: HttpError,
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	const statusCode = error.statusCode || 500;
+
+	return res.status(statusCode).json({
+		message: error.message,
+		stack: config.env === "development" ? error.stack : "",
+	});
+};
+
+export default globalErrorHandler;
