52b8f0dab672cb863461d30627e94939319b9d2c
Suraj-Everlink <s.adhikari@everlink-corp.com>
2025-11-23 21:11:09 +0900

    configure cloudinary

---
+++ src/config/cloudinary.ts
@@ -0,0 +1,11 @@
+import { v2 as cloudinary } from "cloudinary";
+import config from "./config";
+
+// Configure Cloudinary
+cloudinary.config({
+	cloud_name: config.cloudinary_name,
+	api_key: config.cloudinary_api_key,
+	api_secret: config.cloudinary_api_secret,
+});
+
+export default cloudinary;
