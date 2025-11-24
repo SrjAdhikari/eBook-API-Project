862a080474d0a99926730d3ebdac83f2cab7d131
SrjAdhikari <surajadhikari055@gmail.com>
2025-11-24 15:42:41 +0900

    add Cloudinary upload and implement createBook controller

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,11 +1,18 @@
 //* src/book/book.controller.ts
 
 import { Request, Response, NextFunction } from "express";
+import { Book as bookType } from "./book.types";
+import { AuthRequest } from "../middlewares/auth.middleware";
+
 import path from "node:path";
+import fs from "node:fs";
 import cloudinary from "../config/cloudinary";
 import createHttpError from "http-errors";
+import Book from "./book.model";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
+	const { title, genre } = req.body;
+
 	// Multer attaches files in req.files
 	const files = req.files as { [fieldname: string]: Express.Multer.File[] };
 
@@ -22,18 +29,13 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
 	);
 
 	// Upload book cover image to Cloudinary
+	let coverImageUploadResult;
 	try {
-		const coverImageUploadResult = await cloudinary.uploader.upload(filePath, {
+		coverImageUploadResult = await cloudinary.uploader.upload(filePath, {
 			filename_override: fileName,
 			folder: "book_covers",
 			format: coverImageMimeType,
 		});
-
-		console.log(
-			`Cover image uploaded to Cloudinary: ${JSON.stringify(
-				coverImageUploadResult
-			)}`
-		);
 	} catch (error) {
 		const err = createHttpError(
 			500,
@@ -55,24 +57,47 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
 	);
 
 	// Upload book PDF file to Cloudinary
+	let bookUploadResult;
 	try {
-		const bookUploadResult = await cloudinary.uploader.upload(bookFilePath, {
+		bookUploadResult = await cloudinary.uploader.upload(bookFilePath, {
 			resource_type: "raw",
 			filename_override: bookFileName,
 			folder: "book_pdfs",
 			format: bookFileMimeType,
 		});
-
-		console.log(
-			`Book PDF file uploaded to Cloudinary: ${JSON.stringify(
-				bookUploadResult
-			)}`
-		);
 	} catch (error) {
 		const err = createHttpError(
 			500,
 			"Error occurred while uploading book PDF file"
 		);
+		return next(err);
+	}
+
+	// database operation and response
+	let newBook: bookType;
+	const _req = req as AuthRequest;
+	try {
+		newBook = await Book.create({
+			title,
+			genre,
+			author: _req.userId,
+			coverImage: coverImageUploadResult.secure_url,
+			file: bookUploadResult.secure_url,
+		});
+
+		// delete files from local uploads folder
+		await fs.promises.unlink(filePath);
+		await fs.promises.unlink(bookFilePath);
+
+		// send response
+		res.status(201).json({
+			id: newBook._id,
+			message: "New book created successfully",
+		});
+	} catch (error) {
+		console.log(`Error occurred while creating book: ${error}`);
+
+		const err = createHttpError(500, "Error occurred while creating book");
 		return next(err);
 	}
 };
