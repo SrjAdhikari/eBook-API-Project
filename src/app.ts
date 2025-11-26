bacca7234c9597f75b3625f585b40ecb5071c2e7
SrjAdhikari <surajadhikari055@gmail.com>
2025-11-26 19:39:57 +0900

    implement CORS middleware with configurable frontend origin

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,27 @@
 //* src/app.ts
 
 import express from "express";
+import cors from "cors";
 import globalErrorHandler from "./middlewares/error.middleware";
 import userRouter from "./user/user.route";
 import bookRouter from "./book/book.route";
+import config from "./config/config";
 
 const app = express();
+const frontend_url = config.frontend_url as string;
 
 // Parse JSON request body
 app.use(express.json());
+
+// Enable CORS for all routes
+app.use(
+	cors({
+		origin: [frontend_url],
+		methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+		credentials: true,
+		allowedHeaders: ["Content-Type", "Authorization"],
+	})
+);
 
 // ---- Home Route -----
 app.get("/", (req, res, next) => {
